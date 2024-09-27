6d95efd92e3ae93e4e395f115116e2435fe79e80
Ingolf Wagner <contact@ingolf-wagner.de>
2024-09-27 14:33:47 +0900

    :sparkles: add tasksh

--- flake.nix
+++ flake.nix
@@ -6,15 +6,16 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+    taskshell.url = "github:mrvandalo/taskshell"; # tasksh without hardcoded color codes
+    taskshell.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
+    inputs@{ flake-parts, taskshell, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         ./nix/formatter.nix
         ./nix/devshells.nix
-        ./pkgs
       ];
       systems = [
         "x86_64-linux"
@@ -22,6 +23,15 @@
         "aarch64-darwin"
         "x86_64-darwin"
       ];
+
+      perSystem =
+        { pkgs, system, ... }:
+        with pkgs;
+        {
+          packages.tasksh = taskshell.packages.${system}.default;
+          packages.taskwarrior-hooks = callPackage ./pkgs/taskwarrior-hooks { };
+          packages.bugwarrior = callPackage ./pkgs/bugwarrior { };
+        };
       flake = {
         # The usual flake attributes can be defined here, including system-
         # agnostic ones like nixosModule and system-enumerating ones, although
